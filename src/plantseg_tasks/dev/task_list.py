ddfbe373b923edc9f8e0522119cba90c8885fd25
lorenzocerrone <lorenzo.cerrone@gmail.com>
2024-09-05 20:47:31 +0200

    adjust resources for each task

--- src/plantseg_tasks/dev/task_list.py
+++ src/plantseg_tasks/dev/task_list.py
@@ -6,16 +6,16 @@ TASK_LIST = [
     NonParallelTask(
         name="H5 Converter Task",
         executable="convert_h5_to_ome_zarr.py",
-        meta={"cpus_per_task": 1, "mem": 4000},
+        meta={"cpus_per_task": 1, "mem": 8000},
     ),
     NonParallelTask(
         name="Tiff Converter Task",
         executable="convert_tiff_to_ome_zarr.py",
-        meta={"cpus_per_task": 1, "mem": 4000},
+        meta={"cpus_per_task": 1, "mem": 8000},
     ),
     ParallelTask(
         name="PlantSeg Workflow Task",
         executable="plantseg_workflow.py",
-        meta={"cpus_per_task": 1, "mem": 4000},
+        meta={"cpus_per_task": 1, "mem": 32000, "needs_gpu": True},
     ),
 ]
