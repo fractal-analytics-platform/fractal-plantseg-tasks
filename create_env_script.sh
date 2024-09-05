df5dfe0d1310cd7ef4969d9a85c7eb231d692dc3
lorenzo <lorenzo.cerrone@uzh.ch>
2024-09-05 18:11:33 +0200

    add prefix to installation script

--- create_env_script.sh
+++ create_env_script.sh
@@ -1,10 +1,14 @@
 VERSION="v0.1.0"
 COMMMAND="mamba"
 CUDA="CPU" # Available options: 12.1, 11.8 or CPU
+# If ENVPREFIX is not NULL, the environment will be created with the prefix $ENVPREFIX/$ENVNAME 
+# If ENVPREFIX is NULL, the environment will be created in the default location
+ENVPREFIX="NULL" 
 
 # Do NOT change the following lines
-
 ENVNAME=plant-seg-$VERSION
+PYTHON="python=3.11"
+PLANTSEG="plant-seg=1.8.1"
 
 if [ "$CUDA" == "CPU" ]; then
     PYTORCH_PACKAGE="pytorch cpuonly"
@@ -12,11 +16,17 @@ else
     PYTORCH_PACKAGE=pytorch pytorch-cuda=$CUDA
 fi
 
+if [ "$ENVPREFIX" == "NULL" ]; then
+    LOCATION="--name $ENVNAME"
+else
+    LOCATION="--prefix $ENVPREFIX/$ENVNAME"
+fi
+
 echo "Creating environment $ENVNAME with $PYTORCH_PACKAGE"
-$COMMMAND run $COMMMAND create --name $ENVNAME \
+$COMMMAND run $COMMMAND create $LOCATION \
                                -c pytorch \
                                -c nvidia  \
-                               -c conda-forge python=3.11 $PYTORCH_PACKAGE pyqt plant-seg==1.8.1 bioimageio.core \
+                               -c conda-forge $PYTHON $PYTORCH_PACKAGE pyqt $PLANTSEG bioimageio.core \
                                --no-channel-priority -y
 
 echo "Installing plantseg-tasks version $VERSION"
