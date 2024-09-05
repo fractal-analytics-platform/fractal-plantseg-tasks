5646b2444f823203d882828b66d498e23a6c2a85
lorenzo <lorenzo.cerrone@uzh.ch>
2024-09-05 13:04:46 +0200

    update install script

--- create_env_script.sh
+++ create_env_script.sh
@@ -1,16 +1,23 @@
 VERSION="0.1.0"
 COMMMAND="mamba"
-CUDA = "12.1"
+CUDA="CPU" # Available options: 12.1, 11.8 or CPU
 
-echo $(which python)
+# Do NOT change the following lines
 
 ENVNAME=plant-seg-$VERSION
+
+if [ "$CUDA" == "CPU" ]; then
+    PYTORCH_PACKAGE="pytorch cpuonly"
+else
+    PYTORCH_PACKAGE=pytorch pytorch-cuda=$CUDA
+fi
+
+echo "Creating environment $ENVNAME with $PYTORCH_PACKAGE"
 $COMMMAND run $COMMMAND create --name $ENVNAME \
                                -c pytorch \
                                -c nvidia  \
-                               -c conda-forge \ 
-                               pytorch pytorch-cuda=$CUDA pyqt plant-seg bioimageio.core \
-                               --no-channel-priority \
-                               -y
+                               -c conda-forge python=3.11 $PYTORCH_PACKAGE pyqt plant-seg==1.8.1 bioimageio.core \
+                               --no-channel-priority -y
 
+echo "Installing plantseg-tasks version $VERSION"
 $COMMMAND run --name $ENVNAME pip install git+https://github.com/fractal-analytics-platform/fractal-plantseg-tasks@$VERSION
