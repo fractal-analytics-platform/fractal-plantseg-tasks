7e16d9282978963c9689142da233cf152656660d
lorenzocerrone <lorenzo.cerrone@gmail.com>
2024-09-05 20:34:56 +0200

    update interface testing

--- tests/test_valid_task_interface.py
+++ tests/test_valid_task_interface.py
@@ -1,7 +1,6 @@
 import json
 import subprocess
 from shlex import split as shlex_split
-from subprocess import PIPE
 
 import pytest
 from devtools import debug
@@ -16,8 +15,7 @@ def validate_command(cmd: str):
     debug(cmd)
     result = subprocess.run(  # nosec
         shlex_split(cmd),
-        stdout=PIPE,
-        stderr=PIPE,
+        capture_output=True,
     )
     # The command must always fail, since tmp_file_args includes invalid
     # arguments
@@ -25,7 +23,7 @@ def validate_command(cmd: str):
     stderr = result.stderr.decode()
     debug(stderr)
 
-    # Valid stderr includes pydantic.error_wrappers.ValidationError (type
+    # Valid stderr includes pydantic.v1.error_wrappers.ValidationError (type
     # match between model and function, but tmp_file_args has wrong arguments)
     assert "ValidationError" in stderr
 
@@ -46,7 +44,7 @@ def test_task_interface(task, tmp_path):
     tmp_file_args = str(tmp_path / "args.json")
     tmp_file_metadiff = str(tmp_path / "metadiff.json")
     with open(tmp_file_args, "w") as fout:
-        args = dict(wrong_arg_1=123, wrong_arg_2=[1, 2, 3])
+        args = {"wrong_arg_1": 123, "wrong_arg_2": [1, 2, 3]}
         json.dump(args, fout, indent=4)
 
     for key in ["executable_non_parallel", "executable_parallel"]:
