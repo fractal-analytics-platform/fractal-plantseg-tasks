b5edc4edd0e41e32c127c6bb017faaa66b9824f4
lorenzo <lorenzo.cerrone@uzh.ch>
2025-02-12 18:02:00 +0100

    push new tiff sample

--- run_convert_tiff_task.py
+++ run_convert_tiff_task.py
@@ -3,6 +3,6 @@ from src.plantseg_tasks.convert_tiff_to_ome_zarr import convert_tiff_to_ome_zarr
 convert_tiff_to_ome_zarr(
     zarr_urls=[],
     zarr_dir="./ome-zarr/",
-    image_path="./sample_image.tif",
+    image_path="./tests/test.tiff",
     image_layout="YX",
 )
