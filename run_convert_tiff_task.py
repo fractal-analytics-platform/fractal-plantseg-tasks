d9fcf29813dea05a3121b51cb7678ba89432ac6c
lorenzo <lorenzo.cerrone@uzh.ch>
2025-02-12 17:59:14 +0100

    remove wrong png

--- run_convert_tiff_task.py
+++ run_convert_tiff_task.py
@@ -3,6 +3,6 @@ from src.plantseg_tasks.convert_tiff_to_ome_zarr import convert_tiff_to_ome_zarr
 convert_tiff_to_ome_zarr(
     zarr_urls=[],
     zarr_dir="./ome-zarr/",
-    image_path="./2d_cells_apoptotic_nuclei_export.tiff",
+    image_path="./sample_image.tif",
     image_layout="YX",
 )
